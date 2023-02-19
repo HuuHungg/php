fb1d80f64b9d85fd663bc1b8ec89d6286283d480
Huu Hung <huuhungnguyen2002@gmail.com>
2023-02-19 17:39:33 +0700

    add file modifile

---
+++ hello.php
@@ -0,0 +1,31 @@
+<?php
+$customerName = 'Huu Hung';
+$customerDetailEmail = 'huuhungnguyen2002@gmail.com';
+$customerAge = 21;
+echo $customerName;
+echo '<br/>';
+echo 'Hoc lap trinh tai unicode';
+echo '<br/>';
+echo $customerDetailEmail;
+echo '<br/>';
+echo $customerAge = 29;
+echo '<br/>';
+echo $customerAge;
+echo '<br/>';
+echo 50;
+echo '<br/>';
+
+// 
+
+/*
+Ghi chu dong 1
+Ghi chu dong 2
+*/
+// var_dump($var): Trả về kiểu dữ liệu và giá trị
+var_dump($customerAge);
+// print_r($var): Thường dùng để in mảng và đối tượng
+
+echo '<br/>';
+print_r($customerName);
+
+
